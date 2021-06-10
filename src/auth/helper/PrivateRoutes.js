2462d6ce478be0685634d46019bda6db149ecf90
Pravin Sharma <pravinsharma9610@gmail.com>
2021-06-10 20:16:35 +0530

    Added PrivateRoute Component

---
+++ src/auth/helper/PrivateRoutes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { isAuthenticated } from './index';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    console.log("Inside Private Route");
+
+    return (
+        <Route
+            {...rest}
+            render={props => 
+                isAuthenticated() ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to={{
+                        pathname: "/signin",
+                        state: { from: props.location }
+                    }}
+                    />
+                )
+            }
+        />
+    )
+}
+
+export default PrivateRoute
