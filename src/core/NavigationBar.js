cc5447eaa1da67ddf2918f7b19b4bcb36c56acd9
Pravin Sharma <pravinsharma9610@gmail.com>
2021-06-10 20:17:28 +0530

    Made NavBar reactive

--- src/core/NavigationBar.js
+++ src/core/NavigationBar.js
@@ -1,55 +1,64 @@
 import React from 'react'
 import { Link, withRouter } from "react-router-dom";
+import { isAuthenticated, signout } from '../auth/helper';
 
-const currentTab = (history, path) =>{
-    if(history.location.pathname === path){
-        return {color: "green"}
-    }else{
-        return {color: "white"}
+const currentTab = (history, path) => {
+    if (history.location.pathname === path) {
+        return { color: "green" }
+    } else {
+        return { color: "white" }
     }
 }
 
-const NavigationBar = ({history}) => {
+const NavigationBar = ({ history }) => {
     return (
         <div>
             <ul className="nav nav-tabs bg-dark">
                 <li className="nav-item">
-                    <Link style={currentTab(history,'/')} className='nav-link' to='/'>
+                    <Link style={currentTab(history, '/')} className='nav-link' to='/'>
                         Home
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link style={currentTab(history,'/cart')} className='nav-link' to='/cart'>
+                    <Link style={currentTab(history, '/cart')} className='nav-link' to='/cart'>
                         Cart
                     </Link>
                 </li>
-                <li className="nav-item">
-                    <Link style={currentTab(history,'/user/dashboard')} className='nav-link' to='/user/dashboard'>
-                        Dashboard
+                {isAuthenticated() && isAuthenticated().user.role == 0
+                    && <li className="nav-item">
+                        <Link style={currentTab(history, '/user/dashboard')} className='nav-link' to='/user/dashboard'>
+                            User Dashboard
                     </Link>
-                </li>
-                <li className="nav-item">
-                    <Link style={currentTab(history,'/admin/dashboard')} className='nav-link' to='/admin/dashboard'>
-                        A. Dashboard
+                    </li>}
+                {isAuthenticated() && isAuthenticated().user.role == 1
+                    && <li className="nav-item">
+                        <Link style={currentTab(history, '/admin/dashboard')} className='nav-link' to='/admin/dashboard'>
+                            Admin Dashboard
                     </Link>
-                </li>
-                <li className="nav-item">
-                    <Link style={currentTab(history,'/signup')} className='nav-link' to='/signup'>
+                    </li>}
+                {!isAuthenticated() && <li className="nav-item">
+                    <Link style={currentTab(history, '/signup')} className='nav-link' to='/signup'>
                         Sign up
                     </Link>
-                </li>
-                <li className="nav-item">
-                    <Link style={currentTab(history,'/signin')} className='nav-link' to='/signin'>
+                </li>}
+                {!isAuthenticated() && <li className="nav-item">
+                    <Link style={currentTab(history, '/signin')} className='nav-link' to='/signin'>
                         Sign in
                     </Link>
-                </li>
-                <li className="nav-item">
-                    <Link style={currentTab(history,'/signout')} className='nav-link' to='/signout'>
+                </li>}
+
+                {isAuthenticated() && <li className="nav-item">
+                    <div style={currentTab(history, '/signout')} className='nav-link text-warning'
+                        onClick={() => {
+                            signout(() => {
+                                history.push('/signin')
+                            })
+                        }}>
                         Sign out
-                    </Link>
-                </li>
+                    </div>
+                </li>}
             </ul>
-            
+
         </div>
     )
 }
