586c5e519ee61cc07a2d08354865910c77fb1cb5
Pravin Sharma <pravinsharma9610@gmail.com>
2021-06-10 20:18:58 +0530

    Added dummy user dashboard poage

---
+++ src/user/UserDashBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Base from '../core/Base';
+
+const UserDashBoard = () => {
+    console.log("Inside User Dashboard");
+    return (
+        <Base title='User Dashboard'>
+            <h1>User Dashboard</h1>
+        </Base>
+    );
+}
+
+export default UserDashBoard;
