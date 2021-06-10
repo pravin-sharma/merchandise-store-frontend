f67bc5f4afeef9b681bc20c89d76cd70c8ea5154
Pravin Sharma <pravinsharma9610@gmail.com>
2021-06-10 20:17:05 +0530

    Added comment

--- src/core/Base.js
+++ src/core/Base.js
@@ -9,7 +9,10 @@ const Base = ({
 }) => (
 
     <div>
+
         <NavigationBar />
+
+        {/* Children Componnents */}
         <div className="container-fluid">
             <div className="jumbotron bg-dark text-white text-center">
                 <h2 className="display-4">{title}</h2>
