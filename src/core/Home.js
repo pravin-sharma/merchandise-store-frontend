bec3b029d6595d4758f0cf1bb180cc2ba02432e3
Pravin Sharma <pravinsharma9610@gmail.com>
2021-06-18 11:13:34 +0530

    added card components and products loading logic

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../styles.css'
 import API_URL from '../backend'
 import Base from './Base';
+import Card from './Card';
+import { getAllProducts } from './helper/coreapicalls';
 
+const Home = () => {
 
-console.log(API_URL)
-const Home = () =>{
-    return (<Base title="Home Page" description="All the products">
-        <h1>Thsirts..........</h1>
-    </Base>)
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadAllProducts = () => {
+        getAllProducts().then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProducts(data.products)
+            }
+        })
+    }
+
+    useEffect(() => {
+        loadAllProducts()
+    }, []);
+
+    return (
+        <Base title="Home Page" description="All the products">
+            <div className="row text-center">
+                <h1 className="text-white">All Tshirts..</h1>
+                <div className="row">
+                    {products.length > 0 ? (products?.map((product, index) => (
+                        <div key={index} className="col-4">
+                            <Card product={product} />
+                        </div>
+                    ))) : (
+                        <div className="alert alert-info mt-5" role="alert">
+                            Loading Awesome Tshirts for you...
+                        </div>
+                        )
+                    }
+                </div>
+            </div>
+
+        </Base>
+    )
 }
 
 export default Home
